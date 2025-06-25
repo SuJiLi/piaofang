2f0c599aa4a504c938ee488c4acfbb00ba256868
sujili <840661624@qq.com>
2025-06-25 16:02:35 +0800

    同步资源

--- core_gameplay/chonglian.air/chonglian.py
+++ core_gameplay/chonglian.air/chonglian.py
@@ -8,8 +8,9 @@ dev = connect_device("Android:///Q2NVB21806000861")
 touch(Template(r"tpl1749109060222.png", threshold=0.3, record_pos=(0.276, -0.902), resolution=(1080, 2240)))
 sleep(5)
 touch(Template(r"tpl1749109092661.png", threshold=0.3, record_pos=(0.181, 0.639), resolution=(1080, 2240)))
-pic=wait(Template(r"tpl1750323713818.png", record_pos=(-0.006, 0.531), resolution=(1080, 2400)))
+pic=wait(Template(r"tpl1750323713818.png", threshold=0.5, record_pos=(-0.006, 0.531), resolution=(1080, 2400)))
 if pic:
+    sleep(5)
     touch((531,1805))
 sleep(50)
 if wait(Template(r"tpl1749109161160.png")):
