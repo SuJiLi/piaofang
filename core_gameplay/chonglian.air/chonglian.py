452ab75033dac5c810d424c0577e883c8a3505bd
sujili <840661624@qq.com>
2025-06-25 17:35:25 +0800

    同步资源

--- core_gameplay/chonglian.air/chonglian.py
+++ core_gameplay/chonglian.air/chonglian.py
@@ -5,16 +5,16 @@ from airtest.core.api import *
 
 dev = connect_device("Android:///Q2NVB21806000861")
 
-touch(Template(r"tpl1749109060222.png", threshold=0.3, record_pos=(0.276, -0.902), resolution=(1080, 2240)))
+touch((839,137))
 sleep(5)
 touch(Template(r"tpl1749109092661.png", threshold=0.3, record_pos=(0.181, 0.639), resolution=(1080, 2240)))
-pic=wait(Template(r"tpl1750323713818.png", threshold=0.5, record_pos=(-0.006, 0.531), resolution=(1080, 2400)))
+pic=wait(Template(r"tpl1750843426933.png", record_pos=(-0.012, -0.592), resolution=(1080, 2400)))
+
 if pic:
-    sleep(5)
+    sleep(10)
     touch((531,1805))
 sleep(50)
-if wait(Template(r"tpl1749109161160.png")):
-    print("已重启")
+
     
 
 
