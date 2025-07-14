9e58e8849e7faa3ca1752762b102bec04e140cd2
sujili <840661624@qq.com>
2025-07-14 17:32:44 +0800

    优化脚本

--- core_gameplay/chonglian.air/chonglian.py
+++ core_gameplay/chonglian.air/chonglian.py
@@ -8,11 +8,11 @@ dev = connect_device("Android:///Q2NVB21806000861")
 touch((839,137))
 sleep(5)
 touch(Template(r"tpl1750904016357.png", threshold=0.5, record_pos=(0.177, 0.708), resolution=(1080, 2400)))
-pic=wait(Template(r"tpl1750843426933.png", record_pos=(-0.012, -0.592), resolution=(1080, 2400)))
+pic=wait(Template(r"tpl1752485256318.png", record_pos=(-0.393, -0.888), resolution=(1080, 2400)))
 
 if pic:
     sleep(20)
-    touch((531,1805))
+    touch((531,1618))
 sleep(50)
 
     
